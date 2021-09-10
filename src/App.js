bc96e519354bff7b1616dd9193807e5676e6294b
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2021-09-10 14:17:28 -0500

    adds the Add todo func

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import React from 'react';
+import TodoForm from './components/TodoForm';
 function App() {
   return (
     <div className="App">
-      Yellowww
+      <TodoForm />
     </div>
   );
 }
